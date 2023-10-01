ca7e324445e7a49c1db4ace6cf438550992f4f11
Pantal-pl <radek.selewski@wp.pl>
2023-10-01 15:35:25 +0200

    Compleated sql init file for PSQL DB

--- db/sql/init-postgres.sql
+++ db/sql/init-postgres.sql
@@ -1,47 +1,62 @@
-\c weapon_database;
+CREATE TABLE
+    WeaponModel (
+        weapon_model_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        model_name VARCHAR(255) NOT NULL,
+        weapon_type VARCHAR(255),
+        manufacturer VARCHAR(255),
+        model_description TEXT
+    );
 
-CREATE TABLE users
-(
-  uniq UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  id UUID DEFAULT gen_random_UUID(),
-  name VARCHAR(255),
-  surname VARCHAR(255)
-);
+CREATE TABLE
+    Component (
+        component_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        component_name VARCHAR(255) NOT NULL,
+        component_description TEXT,
+        unit_price DECIMAL(10, 2), -- Using DECIMAL for precise financial calculations
+        in_stock INT
+    );
 
-CREATE TABLE WeaponModel
-(
-    model_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    model_name VARCHAR(255), 
-    weapon_type VARCHAR(255),
-    manufacturer VARCHAR(255), 
-    model_description VARCHAR(255)
+CREATE TABLE
+    ModelComponent (
+        entry_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        weapon_weapon_model_id UUID NOT NULL,
+        component_id UUID NOT NULL,
+        quantity_needed_for_model INT,
+        FOREIGN KEY (weapon_weapon_model_id) REFERENCES WeaponModel (weapon_model_id),
+        FOREIGN KEY (component_id) REFERENCES Component (component_id)
+    );
 
-);
+CREATE TABLE
+    Employee (
+        employee_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        employee_first_name VARCHAR(255) NOT NULL,
+        employee_second_name VARCHAR(255),
+        position VARCHAR(255),
+        employment_date DATE,
+        salary DECIMAL(10, 2), -- Using DECIMAL for precise financial calculations
+        employee_contact VARCHAR(255)
+    );
 
-CREATE TABLE ModelComponent
-(
-    entry_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    weapon_model_id UUID,
-    component_id UUID, 
-    quantity_needed_for_model INT
+CREATE TABLE
+    Production (
+        production_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        weapon_weapon_model_id UUID NOT NULL,
+        estimated_completion_date DATE,
+        production_status VARCHAR(255),
+        responsible_employee_id UUID,
+        FOREIGN KEY (weapon_weapon_model_id) REFERENCES WeaponModel (weapon_model_id),
+        FOREIGN KEY (responsible_employee_id) REFERENCES Employee (employee_id)
+    );
 
-);
-
-CREATE TABLE Component
-(
-    component_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    
-
-);
-CREATE TABLE Order_
-(
-
-);
-CREATE TABLE Production
-(
-
-);
-CREATE TABLE Employee
-(
-
-);
+CREATE TABLE
+    Order_ (
+        order_id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
+        order_date DATE,
+        expected_delivery_date DATE,
+        order_status VARCHAR(255),
+        total_order_cost DECIMAL(10, 2), -- Using DECIMAL for precise financial calculations
+        weapon_weapon_model_id UUID NOT NULL,
+        production_id UUID,
+        FOREIGN KEY (weapon_weapon_model_id) REFERENCES WeaponModel (weapon_model_id),
+        FOREIGN KEY (production_id) REFERENCES Production (production_id)
+    );
